c619726d13cad195949020e9557c1ead80c0ac6e
Pi4a <piralith@outlook.fr>
2024-02-09 18:06:10 +0100

    adding code

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,14 @@
 import logo from "../../assets/images/logo.svg"
+import menuopen from "../../assets/images/icon-menu.svg"
+import menuclose from "../../assets/images/icon-menu-close.svg"
+import { useState } from "react"
 
 function Header() {
+  let [isMenuOpen, setMenu] = useState(false)
+
   return (
     <>
-      <div className="header hidden xl-flex justify-between w-4/5 py-12 m-auto">
+      <div className="header hidden lg:flex justify-between w-4/5 py-12 m-auto">
         <span>
           <img src={logo} className="cursor-pointer" />
         </span>
@@ -17,12 +22,36 @@ function Header() {
           </ul>
         </nav>
       </div>
-      <div className="header flexxl-hidden justify-between w-4/5 py-12 m-auto">
+      <div className="header flex lg:hidden justify-between w-11/12 m-auto py-12  overflow-hidden">
         <span>
           <img src={logo} className="cursor-pointer" />
         </span>
-        <nav className="navbar">
-          <ul className="flex flex-col gap-4 font-regular cursor-pointer">
+        <button
+          className=" "
+          onClick={() => {
+            setMenu(!isMenuOpen)
+            console.log(isMenuOpen)
+          }}
+        >
+          <img src={menuopen} />
+        </button>
+        <nav
+          className={
+            !isMenuOpen
+              ? "w-[0px] invisible absolute text-white"
+              : "origin-right w-2/3 duration-500 ease-out bg-white z-10 h-full absolute right-0"
+          }
+        >
+          <button
+            className={isMenuOpen ? "fixed top-[55px] right-5" : "hidden"}
+            onClick={() => {
+              setMenu(!isMenuOpen)
+              console.log(isMenuOpen)
+            }}
+          >
+            <img src={menuclose} />
+          </button>
+          <ul className="flex flex-col gap-7 font-regular cursor-pointer items-baseline px-7 justify-center h-3/5 font-bld text-lg">
             <li>Home</li>
             <li>New</li>
             <li>Popular</li>
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -6,18 +6,25 @@ import webmobile from "../../assets/images/image-web-3-mobile.jpg"
 
 function Home() {
   return (
-    <div className="home flex flex-col gap-6 w-full m-auto">
-      <div className="gridtop gap-7 w-11/12 xl:w-4/5 grid grid-cols-1 grid-rows-1 xl:grid-cols-3 xl:grid-rows-4 xl:gap-x-7 xl:gap-y-12 m-auto">
-        <div className="xl:col-start-1 xl:col-end-3 xl:row-start-1 xl:row-end-1">
-          <img src={webdesktop} className="object-cover hidden xl:block" />
-          <img src={webmobile} className="object-cover xl:hidden block" />
+    <div className="home flex flex-col gap-6 w-full m-auto lg:grid lg:grid-row-3 lg:grid-cols-3">
+      <div
+        className="gridtop lg:min-h-[1000px] gap-7 w-11/12 lg:w-4/5 grid grid-cols-1 grid-rows-1
+         lg:grid-cols-3 lg:grid-rows-3 lg:gap-x-7 lg:gap-y-12 
+          lg:col-start-1 lg:col-end-4 lg:row-start-1 lg:row-end-3 m-auto"
+      >
+        <div className="lg:col-start-1 lg:col-end-3 lg:row-start-1 lg:row-end-2">
+          <img
+            src={webdesktop}
+            className="object-cover hidden lg:block lg:h-[300px] xl:h-auto"
+          />
+          <img src={webmobile} className="object-cover lg:hidden block" />
         </div>
-        <div className="bigtext text-6xl xl:col-start-1 xl:col-end-1 xl-row-start-2 py-1 xl:hidden block">
-          <h1 className="font-extrabld text-5xl">
+        <div className="bigtext text-6lg lg:col-start-1 lg:col-end-1 lg-row-start-1 py-1 lg:hidden block">
+          <h1 className="font-extrabld text-5lg">
             The Bright Future of Web 3.0?
           </h1>
         </div>
-        <div className="block xl:hidden">
+        <div className="block lg:hidden">
           <p className="pb-12 font-reg text-lg ">
             We dive into the next evolution of the web that claims to put the
             power of the platforms back into the hands of the people. But is it
@@ -28,8 +35,9 @@ function Home() {
           </button>
         </div>
         <div
-          className="blogposts flex flex-col bg-deepblue px-6 mt-12
-         py-9 gap-9 xl:col-start-3 xl:col-end-4 xl:row-start-1 xl:row-end-5"
+          className="blogposts flex flex-col bg-deepblue px-6 mt-12 lg:mt-0
+         py-9 gap-9 lg:col-start-3 lg:col-end-3 lg:row-start-1 lg:row-end-3
+         lg:min-h-[500px] lg:max-h[600px] xl:max-h-[560px] 2xl:max-h-[552px]"
         >
           <h1 className="font-extrabld text-golden text-4xl">New</h1>
           <div className="post text-white border-b-graytext flex flex-col ">
@@ -55,10 +63,10 @@ function Home() {
             </p>
           </div>
         </div>
-        <div className="bigtext text-6xl xl:col-start-1 xl:col-end-1 xl-row-start-2 py-1 hidden xl:block">
+        <div className="bigtext text-6xl lg:col-start-1 lg:col-end-1 lg-row-start-2 lg:pt-[2px] hidden lg:block">
           <h1 className="font-extrabld ">The Bright Future of Web 3.0?</h1>
         </div>
-        <div className="xl:block hidden">
+        <div className="lg:block hidden lg:pt-[75px] xl:pt-0">
           <p className="pb-12 ">
             We dive into the next evolution of the web that claims to put the
             power of the platforms back into the hands of the people. But is it
@@ -68,39 +76,39 @@ function Home() {
             READ MORE
           </button>
         </div>
-      </div>
-      <div className="gridbottom w-11/12 m-auto grid gap-2 pt-10">
-        <div className="Articles flex py-4 gap-5">
-          <img src={retro} className="w-1/3 pr-2" />
-          <div className="flex flex-col gap-3 ">
-            <h1 className="font-extrabld text-gray-300 text-5xl">01</h1>
-            <h2 className="font-extrabld text-xl">Reviving Retro PCs</h2>
-            <p className="font-reg text-base">
-              What happens when old PCs are given modern upgrades?
-            </p>
+        <div className="gridbottom lg:-mt-5 lg:gap-x-8  m-auto grid gap-2 pt-10 lg:pt-0 xl:-mt-[7.5rem] lg:col-start-1 lg:col-end-4 lg:row-start-3 lg:grid-cols-3 lg:grid-rows-1">
+          <div className="Articles flex py-4 gap-7">
+            <img src={retro} className="w-1/3 pr-2 object-cover" />
+            <div className="flex flex-col gap-3 ">
+              <h1 className="font-extrabld text-gray-300 text-5lg">01</h1>
+              <h2 className="font-extrabld text-lg">Reviving Retro PCs</h2>
+              <p className="font-reg text-base">
+                What happens when old PCs are given modern upgrades?
+              </p>
+            </div>
           </div>
-        </div>
-        <div className="Articles flex py-4 gap-5">
-          <img src={laptops} className="w-1/3 pr-2" />
-          <div className="flex flex-col gap-3 ">
-            <h1 className="font-extrabld text-gray-300 text-5xl">02</h1>
-            <h2 className="font-extrabld text-xl">Top 10 Laptops of 2022</h2>
-            <p className="font-reg text-base">
-              Our best picks for various needs and budgets
-            </p>
+          <div className="Articles flex py-4 gap-7">
+            <img src={laptops} className="w-1/3 pr-2 object-cover" />
+            <div className="flex flex-col gap-3 ">
+              <h1 className="font-extrabld text-gray-300 text-5lg">02</h1>
+              <h2 className="font-extrabld text-lg">Top 10 Laptops of 2022</h2>
+              <p className="font-reg text-base">
+                Our best picks for various needs and budgets
+              </p>
+            </div>
           </div>
-        </div>
-        <div className="Articles flex py-4 gap-5">
-          <img src={gaming} className="w-1/3 pr-2" />
-          <div className="flex flex-col gap-3 ">
-            <h1 className="font-extrabld text-gray-300 text-5xl">03</h1>
-            <h2 className="font-extrabld text-xl">The Growth of Gaming</h2>
-            <p className="font-reg text-base">
-              How the pandemic has sparked fresh opportunities.
-            </p>
+          <div className="Articles flex py-4 gap-7">
+            <img src={gaming} className="w-1/3 pr-2 object-cover" />
+            <div className="flex flex-col gap-3 ">
+              <h1 className="font-extrabld text-gray-300 text-5lg">03</h1>
+              <h2 className="font-extrabld text-lg">The Growth of Gaming</h2>
+              <p className="font-reg text-base">
+                How the pandemic has sparked fresh opportunities.
+              </p>
+            </div>
           </div>
+          <h1></h1>
         </div>
-        <h1></h1>
       </div>
     </div>
   )
